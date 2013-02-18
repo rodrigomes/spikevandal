f4daeb179ef6d52a66b75cd5bcc5d4c08e748f6e
Rodrigo Gomes <rgomes.rodrigo@gmail.com>
2013-02-18 12:18:40 -0300

    Add image validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,6 @@ class Post < ActiveRecord::Base
   attr_accessible :image
   has_attached_file :image, :styles => { :normal => "1008x1152", :medium => "300x300>", :thumb => "100x100>" }
 
+  validates :image, :attachment_presence => true
+
 end
